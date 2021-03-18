bd3541d6929df356156c58d11905267f179927d4
vinh <vinh@localhost.localdomain>
2021-03-19 03:52:59 +0700

    hmm Quoc Anh Nguyen haxor

--- proxy-cmd/main.go
+++ proxy-cmd/main.go
@@ -17,11 +17,15 @@ const cmd = "stdbuf"
 
 var addr string
 var cmdArgs = []string{"-i0", "-o0", "-e0"}
+var stdinAsArg1 = 0 != len(os.Getenv("PROXY_CMD_STDIN_AS_ARG1"))
+
+const maxSTDINSize = 50 * 1024 // 50 KB
+const stdinTimeout = 5 * time.Second
 
 func main() {
 	// setrLimit()
 	if len(os.Args) < 4 {
-		log.Println(`Usage:`, os.Args[0], " working_dir listen_address command args...\r\n\tEg:", os.Args[0], "/opt :9999 pwd")
+		log.Println(`Usage:`, os.Args[0], " working_dir listen_address command args...\r\n\tEg:", os.Args[0], "/opt :9999 pwd\r\n\r\nSet non-empty environ variable PROXY_CMD_STDIN_AS_ARG1 to pass stdin as first arg to prog.")
 		os.Exit(1)
 	}
 
@@ -50,7 +54,49 @@ func main() {
 			log.Println("Accept err:", e)
 			return
 		}
-		go handleConn(conn)
+		if stdinAsArg1 {
+			go handleConn1(conn)
+		} else {
+			go handleConn(conn)
+		}
+	}
+}
+
+func handleConn1(conn net.Conn) {
+	defer func() {
+		time.Sleep(time.Second)
+		conn.Close()
+	}()
+	conn.Write([]byte("We take your input to pass to the program as an argument. We accept binary formats like \xff, except null-character (\\x00). We will wait 5 seconds for you to finish typing.\r\n"))
+	var data []byte
+	lenData := 0
+	buf := make([]byte, 1024)
+	for {
+		conn.SetReadDeadline(time.Now().Add(stdinTimeout))
+		n, err := conn.Read(buf)
+		if n != 0 {
+			if n >= maxSTDINSize-lenData {
+				buf = buf[:maxSTDINSize-lenData]
+				data = append(data, buf...)
+				break
+			}
+			buf = buf[:n]
+			lenData += n
+			data = append(data, buf...)
+		}
+		if err != nil {
+			// log.Println("Read remote:", err)
+			break
+		}
+	}
+	arg1 := string(data)
+	newArgs := append(cmdArgs, arg1)
+	cmd := exec.Command(cmd, newArgs...)
+	cmd.Stdout = conn
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Run cmd:", err)
 	}
 }
 
